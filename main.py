5000d08d5a5fe54204336707c8297587b8ccb90c
garima rokaha <garimarokaha5@gmail.com>
2025-07-16 16:16:22 +0545

    ui

--- main.py
+++ main.py
@@ -1,40 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
-import joblib #model lai load garna ko lagi joblib
+import joblib
 import numpy as np
 
-app = FastAPI()  #app bhane object banayeko, yehi ibject bata route banaune ho
-Model = joblib.load('saved/DecisionTreePrediction.pkl')
+app = FastAPI()
+model = joblib.load('Saved/DecisionTreePrediction.pkl')
 
-#Glucose
-#BloodPressure 
-#DiabetesPedigreeFunction (dfp)
-#Age
+templates = Jinja2Templates(directory="templates")
 
-class Patient(BaseModel): #data banako ra data validation ko lagi pydentic use gareko ho
-    Glucose: float
-    BloodPressure: int
-    dfp : float
-    Age : int 
-    #yo data laii model ma pass garnu parchha aaba. 
+class Patient(BaseModel):
+    gulcose: float
+    bloodpressure: int
+    dfp: float
+    age: int
 
-@app.get('/') #@ ley route banaune
-def show():
-    return {'message':'Hello Guys!!'}
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-
-@app.get('/about')
+@app.get("/about")
 def about():
-    return {'Display':'Broad AI is a education sector'}
+    return {"Display": "Broad AI is the educational sector"}
 
-
-@app.post('/predict')
+@app.post("/predict")
 async def predict(data: Patient):
-    input_data = np.array([[
-        data.Glucose, data.BloodPressure, data.dfp, data.Age
-    ]])
-
-    pred = await Model.predict(input_data)[0]
-    return {'prediction':int(pred)}
-
-    
+    input_data = np.array([[data.gulcose, data.bloodpressure, data.dfp, data.age]])
+    prediction = model.predict(input_data)[0]
+    return {"prediction": int(prediction)}
